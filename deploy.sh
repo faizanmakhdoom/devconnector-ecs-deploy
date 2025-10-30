a6ec5049a341aef4705ea7b47be2515703bb70a4
faizanmmakhdoom <faizan.makhdooom@zigron.com>
2025-10-30 17:01:20 +0500

    new changes

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,12 @@ echo "=== Registering ECS Task Definition ==="
 aws ecs register-task-definition \
   --region us-east-1 \
   --cli-input-json '{
-    "family": "devconnector-task",
-    "requiresCompatibilities": ["EC2"],
-    "networkMode": "bridge",
-    "executionRoleArn": "arn:aws:iam::623653226560:role/ecsTaskExecutionRole",
-    "containerDefinitions": [
-
-        {
+  "family": "devconnector-task",
+  "networkMode": "awsvpc",
+  "requiresCompatibilities": ["EC2"],
+  "executionRoleArn": "arn:aws:iam::623653226560:role/ecsTaskExecutionRole",
+  "containerDefinitions": [
+    {
       "name": "mongodb",
       "image": "mongo:6.0",
       "essential": false,
@@ -24,40 +23,41 @@ aws ecs register-task-definition \
       ],
       "mountPoints": [
         {
-          "sourceVolume": "mongo-data",
+          "sourceVolume": "mongo_data_volume",
           "containerPath": "/data/db"
         }
       ]
     },
-      {
-        "name": "devconnector",
-        "image": "623653226560.dkr.ecr.us-east-1.amazonaws.com/devconnector:latest",
-        "essential": true,
-        "memory": 512,
-        "cpu": 256,
-        "portMappings": [
-          {
-            "containerPort": 5000,
-            "hostPort": 5000,
-            "protocol": "tcp"
-          }
-        ],
-        "environment": [
-          { "name": "NODE_ENV", "value": "production" },
-          { "name": "JWT_SECRET", "value": "supersecretkey" },
-          { "name": "MONGO_URI", "value": "mongodb://172.31.12.45:27017/devconnector" }
-        ],
-        "logConfiguration": {
-          "logDriver": "awslogs",
-          "options": {
-            "awslogs-group": "/ecs/devconnector",
-            "awslogs-region": "us-east-1",
-            "awslogs-stream-prefix": "ecs"
-          }
+    {
+      "name": "devconnector",
+      "image": "623653226560.dkr.ecr.us-east-1.amazonaws.com/devconnector:latest",
+      "essential": true,
+      "memory": 512,
+      "portMappings": [
+        { "containerPort": 5000, "hostPort": 5000 }
+      ],
+      "environment": [
+        { "name": "NODE_ENV", "value": "production" },
+        { "name": "JWT_SECRET", "value": "supersecretkey" },
+        { "name": "MONGO_URI", "value": "mongodb://mongodb:27017/devconnector" }
+      ],
+      "dependsOn": [
+        {
+          "containerName": "mongodb",
+          "condition": "START"
         }
+      ]
+    }
+  ],
+  "volumes": [
+    {
+      "name": "mongo_data_volume",
+      "host": {
+        "sourcePath": "/var/lib/docker/volumes/mongo_data"
       }
-    ]
-  }'
+    }
+  ]
+}'
 
 echo "=== Task Definition Registered Successfully ==="
 
