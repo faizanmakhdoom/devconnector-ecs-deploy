9e68a85478652e39dd6b797c96827bfa541bfda2
faizanmmakhdoom <faizan.makhdooom@zigron.com>
2025-10-30 16:48:13 +0500

    Add axios dependency for frontend build

--- config/db.js
+++ config/db.js
@@ -1,20 +1,16 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
+    const db = process.env.MONGO_URI || 'mongodb://mongodb:27017/devconnector';
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
       useUnifiedTopology: true
     });
 
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    // Exit process with failure
     process.exit(1);
   }
 };
